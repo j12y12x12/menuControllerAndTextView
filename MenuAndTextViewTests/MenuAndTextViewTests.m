c96e96f4e4181832e29c3563f9b7b74c7452c78f
jinyixian <jinyixian@qiyi.com>
2018-08-16 11:58:06 +0800

    修改显示

--- MenuAndTextViewTests/MenuAndTextViewTests.m
+++ MenuAndTextViewTests/MenuAndTextViewTests.m
@@ -2,7 +2,7 @@
 //  MenuAndTextViewTests.m
 //  MenuAndTextViewTests
 //
-//  Created by 金伊贤 on 2018/8/16.
+//  Created by jyx on 2018/8/16.
 //  Copyright © 2018年 jjyyxx. All rights reserved.
 //
 
